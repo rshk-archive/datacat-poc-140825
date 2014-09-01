6224d14afa3acb8e2003d6d9f3b3519dd76dc122
Samuele Santi <redshadow@hackzine.org>
2014-09-01 18:06:13 +0200

    Added some todos

--- datacat/web/__init__.py
+++ datacat/web/__init__.py
@@ -4,3 +4,7 @@ from datacat.web.blueprints.admin import admin_bp
 
 app = flask.Flask(__name__)
 app.register_blueprint(admin_bp, url_prefix='/api/1/admin')
+
+# todo: load configuration + configure app
+# todo: instantiate plugins from configuration
+# todo: register plugin blueprints under appropriate prefix
