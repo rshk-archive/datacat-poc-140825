ac9b71c0c1689245b30e42b3c707d0209a34c021
Samuele Santi <redshadow@hackzine.org>
2014-09-04 11:41:53 +0200

    Some more default settings

--- datacat/settings/default.py
+++ datacat/settings/default.py
@@ -1,4 +1,17 @@
+# ============================================================
+#     Flask configuration
+# ============================================================
+# See: http://flask.pocoo.org/docs/latest/config/#builtin-configuration-values
+
 DEBUG = False
+TESTING = False
+
+# SECRET_KEY = 'This is some secret'
+
+
+# ============================================================
+#     Datacat configuration
+# ============================================================
 
 DATABASE = {
     'database': None,
@@ -13,4 +26,13 @@ PLUGINS = [
 ]
 
 
+# ============================================================
+#     Celery configuration
+# ============================================================
+# See: http://docs.celeryproject.org/en/latest/configuration.html
+
+
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+# CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
+CELERY_ACCEPT_CONTENT = ['json', 'msgpack', 'yaml']
