081ae01cf636d1be2029cc7233fb84bc3feadb50
Samuele Santi <redshadow@hackzine.org>
2014-09-16 19:14:15 +0200

    Better celery settings for testing -- now we're getting exceptions!

--- datacat/settings/testing.py
+++ datacat/settings/testing.py
@@ -1,4 +1,8 @@
 from datacat.settings.default import *  # noqa
 
 # We want this plugin to be included
+PLUGINS = list(PLUGINS)
 PLUGINS.append('datacat.utils.testing.plugins:dummy_plugin')
+
+CELERY_ALWAYS_EAGER = True
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
