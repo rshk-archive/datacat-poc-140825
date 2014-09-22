3885722e2a634b450fb1e8e984be539ed6970a80
Samuele Santi <redshadow@hackzine.org>
2014-09-22 13:38:22 +0200

    Improvements to core plugin

--- datacat/ext/core.py
+++ datacat/ext/core.py
@@ -1,12 +1,15 @@
+from urlparse import urlparse
+
 from flask import url_for
 from werkzeug.exceptions import NotFound
 
-from datacat.db import get_db
+from datacat.db import db
 from datacat.ext.base import Plugin
 from datacat.web.utils import json_view
 
 
 core_plugin = Plugin(__name__)
+core_plugin.__doc__ = """The core plugin"""
 
 
 @core_plugin.hook('make_dataset_metadata')
@@ -17,46 +20,55 @@ def make_dataset_metadata(dataset_id, config, metadata):
     if 'resources' in config:
         metadata['resources'] = []
         for resource_id, resource in enumerate(config['resources']):
+            if isinstance(resource, basestring):
+                resource = {'url': resource}
+
+            resource_url = resource['url']
+            _parsed_url = urlparse(resource_url)
+            if _parsed_url.scheme == 'internal':
+                # We need to replace the URL with a public-facing one
+                # TODO: we should use something more generic here..
+                resource_url = url_for(
+                    'public.serve_resource_data',
+                    resource_id=int(_parsed_url.path.split('/')[1]),
+                    _external=True)
+
             metadata['resources'].append({
-                'url': url_for(__name__ + '.get_dataset_resource',
-                               dataset_id=dataset_id,
-                               resource_id=resource_id,
-                               _external=True)
+                'url': resource_url,
             })
 
 
-@core_plugin.route('/data/<int:dataset_id>/resource/<int:resource_id>')
-@json_view
-def get_dataset_resource(dataset_id, resource_id):
-    db = get_db()
-    with db.cursor() as cur:
-        cur.execute("SELECT id, configuration FROM dataset WHERE id = %(id)s",
-                    dict(id=dataset_id))
-        row = cur.fetchone()
-        if row is None:
-            raise NotFound("The dataset was not found")
-        dataset_conf = row['configuration']
+# @core_plugin.route('/data/<int:dataset_id>/resource/<int:resource_id>')
+# @json_view
+# def get_dataset_resource(dataset_id, resource_id):
+#     with db.cursor() as cur:
+#         cur.execute("SELECT id, configuration FROM dataset WHERE id = %(id)s",
+#                     dict(id=dataset_id))
+#         row = cur.fetchone()
+#         if row is None:
+#             raise NotFound("The dataset was not found")
+#         dataset_conf = row['configuration']
 
-    try:
-        resource_conf = dataset_conf['resources'][resource_id]
+#     try:
+#         resource_conf = dataset_conf['resources'][resource_id]
 
-    except (KeyError, IndexError):
-        raise NotFound("The resource was not found")
+#     except (KeyError, IndexError):
+#         raise NotFound("The resource was not found")
 
-    resource_type = resource_conf.get('type')
-    if resource_type == 'internal':
-        url = url_for('public.serve_resource_data',
-                      resource_id=resource_conf['id'],
-                      _external=True)
+#     resource_type = resource_conf.get('type')
+#     if resource_type == 'internal':
+#         url = url_for('public.serve_resource_data',
+#                       resource_id=resource_conf['id'],
+#                       _external=True)
 
-    elif 'url' in resource_conf:
-        url = resource_conf['url']
+#     elif 'url' in resource_conf:
+#         url = resource_conf['url']
 
-    else:
-        raise NotFound("Unable to find an URL for the resource")
+#     else:
+#         raise NotFound("Unable to find an URL for the resource")
 
-    return '', 302, {'Location': url}
-    # return redirect(url, code=302)
+#     return '', 302, {'Location': url}
+#     # return redirect(url, code=302)
 
 
 @core_plugin.task()
